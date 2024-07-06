e8a0f8476adc0e76fccc0365e726d14f1d973265
周尚礼 <105686154+zsl2021533013@users.noreply.github.com>
2024-07-06 17:25:19 +0800

    接口更改

---
+++ Assets/GameMain/Scripts/Controllers/Character/Player/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using Assets.GameMain.Scripts.Character.BlackHoleLogic;
+using Assets.GameMain.Scripts.Character.Movement;
+using Assets.GameMain.Scripts.Logic.Input;
+using Assets.GameMain.Scripts.Looper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameMain.Scripts.Controllers;
+using UnityEngine;
+
+namespace Assets.GameMain.Scripts.Character.Player
+{
+    public class PlayerController : ControllerBase
+    {
+        private BlackHole mBlackHole;
+
+        private MovementComp mMovementComp;
+
+
+        private void Awake()
+        {
+            mMovementComp = GetComponent<MovementComp>();
+        }
+
+        public void OnUpdate(float eclapse)
+        {
+        }
+
+        private void Update()
+        {
+            mMovementComp.OnUpdate(Time.deltaTime);
+        }
+
+        private void FixedUpdate()
+        {
+            mMovementComp.OnFixedUpdate(Time.fixedDeltaTime);
+        }
+
+        public void OnFixedUpdate(float eclapse)
+        {
+            
+        }
+
+    }
+}
