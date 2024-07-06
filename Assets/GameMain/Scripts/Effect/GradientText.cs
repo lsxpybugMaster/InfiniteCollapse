e129e52f4da3890484d779a054a584f830f0bffe
lsxpybugMaster <1275425660@qq.com>
2024-07-06 20:56:34 +0800

    环形速度条UI和渐变显示教程文字UI

--- Assets/GameMain/Scripts/Effect/GradientText.cs
+++ Assets/GameMain/Scripts/Effect/GradientText.cs
@@ -6,16 +6,36 @@ using DG.Tweening;
 
 public class GradientText : MonoBehaviour
 {
-    public TextMeshProUGUI targetText;
+    TextMeshProUGUI mytext;
+    public float elapsetime;
 
     void Start()
     {
-        // 确保文本的颜色初始值是透明的
-        Color color = targetText.color;
-        color.a = 0f;
-        targetText.color = color;
+        mytext = GetComponent<TextMeshProUGUI>();
+        showTextMesh();
+        Invoke(nameof(clearTextMesh),elapsetime);
+    }
 
-        // 使用DOTween使文本在2秒内逐渐浮现
-        targetText.DOFade(1f, 2f);
+    //逐渐展示文字
+    void showTextMesh()
+    {
+        Color color = mytext.color;
+        color.a = 0f;
+        mytext.color = color;
+
+      
+        mytext.DOFade(1f, 3f);
+    }
+
+    //逐渐消去文字
+    void clearTextMesh()
+    {
+        mytext.DOFade(0f, 1.5f);
+        Invoke(nameof(Destroy), 3f);
+    }
+
+    void Destroyme()
+    {
+        Destroy(gameObject);
     }
 }
