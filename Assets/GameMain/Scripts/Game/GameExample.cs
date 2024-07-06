214077c53c067287bb899bc299e5d6e2fe6adb71
lsxpybugMaster <1275425660@qq.com>
2024-07-06 12:53:42 +0800

    Root

---
+++ Assets/GameMain/Scripts/Game/GameExample.cs
@@ -0,0 +1,29 @@
+﻿using Assets.GameMain.Scripts.Looper;
+using QFramework;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace GameMain.Scripts.Game
+{
+    public class GameLoopMain : GameBase, IController
+    {
+        public List<ILooper> Loopers;
+
+
+        public override void Initialize()
+        {
+            //Loopers = 
+        }
+
+        public override void Update(float elapse)
+        {
+            Loopers.ForEach(x => x.OnUpdate(elapse));
+        }
+
+        public IArchitecture GetArchitecture()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
