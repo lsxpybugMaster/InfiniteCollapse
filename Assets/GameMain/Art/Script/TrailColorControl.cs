bed6de33249859b592581b10e784d54dd92dab79
LawrenceShaw <lawrencetheowl@qq.com>
2024-07-07 07:52:59 +0800

    上传了当前完成的粒子效果和shader
    尾迹shader在art/prefab文件夹里，把prefab绑到人物身上再把这个prefab的代码补全就可以实现功能了；粒子特效在art/prefab里，命名首字母对应文档里的字母

---
+++ Assets/GameMain/Art/Script/TrailColorControl.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailColorControl : MonoBehaviour
+{
+    public Material mat;
+    public Color lowSpeedColor;
+    public Color highSpeedColor;
+    private float threshholdSpeed;
+    private float currentSpeed;
+    private TrailRenderer tr;
+    //public float test;
+
+    void Start()
+    {
+        mat = GetComponent<TrailRenderer>().materials[0];
+        tr = GetComponent<TrailRenderer>();
+        tr.materials[0] = mat;
+    }
+
+
+    void Update()
+    {
+        //获取当前玩家速度和通关阈值速度
+        //currentSpeed = 玩家当前速度;
+        //threshold = 当前关卡通关阈值速度;
+        //以下的功能是：玩家的速度大于阈值就改变尾迹颜色   应该不需要修改，把上面的两行获取速度的代码写一下就可以了
+        if (currentSpeed>=threshholdSpeed)
+        {
+            tr.materials[0].SetColor("_Color1", highSpeedColor);
+        }
+        else
+        {
+            tr.materials[0].SetColor("_Color1", lowSpeedColor);
+        }
+    }
+}
